6c80c8ec3b0479ae3ff2db906f9c7591681433e7
Tobias Waslowski <contact@twaslowski.com>
2026-01-31 19:44:27 +0100

    feat: create a separate dev environment

---
+++ terraform/application/config/dev.tfvars
@@ -0,0 +1,7 @@
+environment              = "dev"
+app_domain               = "dev.grammr.app"
+zone_id                  = "fe0c8417e8c4c53f1806fca1bc09017c"
+supabase_project_url     = "https://wgngrrmngatubjsilhbj.supabase.co"
+supabase_publishable_key = "sb_publishable_lpFzYUt6e8OxbLQNPG4SIw_rf2MYDFq"
+
+morphology_lambda_version = "0.1.1"
--- terraform/application/vercel.tf
+++ terraform/application/vercel.tf
@@ -1,8 +1,19 @@
 # Annoyingly, git_branch can only be specified for non-production domains.
 # Therefore, this code creates two separate resources for production and development domains.
 resource "vercel_project_domain" "domain_prod" {
+  count = var.environment == "prod" ? 1 : 0
+
   project_id = data.vercel_project.project.id
   domain     = var.app_domain
+}
+
+resource "vercel_project_domain" "domain_dev" {
+  count = var.environment == "dev" ? 1 : 0
+
+  project_id = data.vercel_project.project.id
+  domain     = var.app_domain
+
+  git_branch = "develop"
 }
 
 resource "vercel_project_environment_variable" "vercel_env" {
